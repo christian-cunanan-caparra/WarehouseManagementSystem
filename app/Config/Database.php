7615c4a1f3de37dd696030c6d357aa171b9f65de
Christian Cunanan Caparra <caparrachristian8@gmail.com>
2025-01-28 01:24:08 +0800

    s

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,7 +27,7 @@ class Database extends Config
     public $default = [
         
         'DSN'      => '',
-        'hostname' => 'ftp://warehousemanagementsystem.shop',
+        'hostname' => '127.0.0.1:3306',
         'username' => 'u988465998',
         'password' => 'Ajdkpe11hjk',
         'database' => 'warehousemanagement',
