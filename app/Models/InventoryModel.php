2bb91929c31e1342cf1d3476eddb550b8ad684a2
Christian Cunanan Caparra <caparrachristian8@gmail.com>
2025-01-28 12:33:30 +0800

    trial by chan

---
+++ app/Models/InventoryModel.php
@@ -0,0 +1,10 @@
+<?php namespace App\Models;
+
+use CodeIgniter\Model;
+
+class InventoryModel extends Model
+{
+    protected $table = 'inventory';
+    protected $primaryKey = 'id';
+    protected $allowedFields = ['name', 'stock'];
+}
