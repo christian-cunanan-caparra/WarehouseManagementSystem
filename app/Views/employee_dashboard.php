c73072e5b44f8290cdd6bdd8e50e1672c2b8e805
Christian Cunanan Caparra <caparrachristian8@gmail.com>
2025-01-29 14:26:27 +0800

    s

--- app/Views/employee_dashboard.php
+++ app/Views/employee_dashboard.php
@@ -147,6 +147,8 @@
             <li><a href="#"><span class="material-icons">dashboard</span> Dashboard</a></li>
             <li><a href="#"><span class="material-icons">inventory</span> Products</a></li>
             <li><a href="/inventory"><span class="material-icons">storage</span> Inventory</a></li>
+            <li><a href="/inventory/logs"><span class="material-icons">history</span> Inventory Logs</a></li>
+
 
         </ul>
     </aside>
