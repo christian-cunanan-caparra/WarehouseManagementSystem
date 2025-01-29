34f6221b8ce7e2d00bd0ac965d9dd5e6ae5c32e4
John Lloyd Guevarra <johnlloydguevarra0405@gmail.com>
2025-01-29 17:49:59 +0800

    s

--- app/Views/inventory_logs.php
+++ app/Views/inventory_logs.php
@@ -4,19 +4,19 @@
     <thead>
         <tr>
             <th>Product Name</th>
-            <th>Action</th>
             <th>Quantity</th>
             <th>Date</th>
+            <th>Action</th>
+
         </tr>
     </thead>
     <tbody>
-        <?php foreach ($logs as $log): ?>
+        
         <tr>
-            <td><?= esc($log['name']) ?></td>
-            <td><?= ($log['action'] == 'Added') ? '<span class="text-success">Stock In</span>' : '<span class="text-danger">Stock Out</span>' ?></td>
-            <td><?= esc($log['quantity']) ?></td>
-            <td><?= esc($log['created_at']) ?></td>
+            <td>Sample</td>
+            <td>Sample</td>
+            <td>Sample</td>
+            <td>Sample</td>
         </tr>
-        <?php endforeach; ?>
     </tbody>
 </table>
