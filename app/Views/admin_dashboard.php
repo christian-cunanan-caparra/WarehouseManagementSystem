1deb1dbe5dee1c8e9bde2a36c830fd1ded1dd6d4
jhuniel-galang <jhunielstorage1@gmail.com>
2025-01-30 12:38:10 +0800

    added routes and add file create_management and some changes in the dashboard admin - Jhuniel

--- app/Views/admin_dashboard.php
+++ app/Views/admin_dashboard.php
@@ -230,7 +230,7 @@
         <div class="sidebar-header">Warehouse Management</div>
         <ul class="sidebar-links">
             <li><a href="/employee_dashboard"><span class="material-icons">dashboard</span> Dashboard</a></li>
-            <li><a href="/product"><span class="material-icons">inventory</span> Account Management </a></li>
+            <li><a href="/account-management"><span class="material-icons">inventory</span> Account Management </a></li>
             <li><a href="/inventory"><span class="material-icons">storage</span> Account Archive</a></li>
         </ul>
     </aside>
