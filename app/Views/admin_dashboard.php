c37eff32220b8cae2e4b04c06e5713525811db98
jhuniel-galang <jhunielstorage1@gmail.com>
2025-01-30 12:22:00 +0800

    Names in sidebar - Jhuniel

--- app/Views/admin_dashboard.php
+++ app/Views/admin_dashboard.php
@@ -230,9 +230,8 @@
         <div class="sidebar-header">Warehouse Management</div>
         <ul class="sidebar-links">
             <li><a href="/employee_dashboard"><span class="material-icons">dashboard</span> Dashboard</a></li>
-            <li><a href="/product"><span class="material-icons">inventory</span> Products</a></li>
-            <li><a href="/inventory"><span class="material-icons">storage</span> Inventory</a></li>
-            <li><a href="/inventory_logs"><span class="material-icons">storage</span> Inventory Logs</a></li>
+            <li><a href="/product"><span class="material-icons">inventory</span> Account Management </a></li>
+            <li><a href="/inventory"><span class="material-icons">storage</span> Account Archive</a></li>
         </ul>
     </aside>
 
