6d1d42e6ddfd260b8a2b0693e070e9a122c44637
deramos-michael <deramosmichael27@gmail.com>
2025-01-30 13:51:07 +0800

    s

--- app/Views/employee_dashboard.php
+++ app/Views/employee_dashboard.php
@@ -207,8 +207,7 @@
             </div>
         </div>
     </div>
-    <br>
-<br>
+   
     <div class="col-md-4">
         <div class="card shadow-sm border-0 bg-primary text-white">
             <div class="card-body d-flex justify-content-between align-items-center">
