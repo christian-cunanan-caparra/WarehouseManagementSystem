1592e3cdcb814f14cf01d8a2615757b2df5ad71b
deramos-michael <deramosmichael27@gmail.com>
2025-01-30 13:49:38 +0800

    s

--- app/Views/employee_dashboard.php
+++ app/Views/employee_dashboard.php
@@ -192,7 +192,34 @@
             </div>
         </div>
     </div>
+    <div class="col-md-4">
+        <div class="card shadow-sm border-0 bg-primary text-white">
+            <div class="card-body d-flex justify-content-between align-items-center">
+                <div>
+                    <h5 class="card-title">Total Stocks Out</h5>
+                    <h3 class="card-text">
+                        <?= array_sum(array_column($products, 'stock_out')) ?>
+                    </h3>
+                </div>
+                <span class="material-icons">inventory</span>
+            </div>
+        </div>
+    </div>
     
+
+    <div class="col-md-4">
+        <div class="card shadow-sm border-0 bg-primary text-white">
+            <div class="card-body d-flex justify-content-between align-items-center">
+                <div>
+                    <h5 class="card-title">Total Stocks In</h5>
+                    <h3 class="card-text">
+                        <?= array_sum(array_column($products, 'stock_in')) ?>
+                    </h3>
+                </div>
+                <span class="material-icons">inventory</span>
+            </div>
+        </div>
+    </div>
 </div>
 
 
