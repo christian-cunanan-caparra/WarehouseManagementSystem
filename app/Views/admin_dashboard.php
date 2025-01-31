426f55d38ce31f11833dd6b4cc0b9b5f9f518d35
Christian Cunanan Caparra <caparrachristian8@gmail.com>
2025-01-31 20:32:00 +0800

    ss
    Signed-off-by: Christian Cunanan Caparra <caparrachristian8@gmail.com>

--- app/Views/admin_dashboard.php
+++ app/Views/admin_dashboard.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
-    <!-- Boosssmtstrap 5 -->
+    <!-- Bosssosssmtdjnnstrap 5 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <!-- Google Icons -->
+    <!-- Googssle Icons -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     <style>
