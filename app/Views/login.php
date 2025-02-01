ac2ccba2b4f9cec92f7071c47a23eb6ae92f00c4
Christian Cunanan Caparra <caparrachristian8@gmail.com>
2025-02-01 13:51:49 +0800

    s

--- app/Views/login.php
+++ app/Views/login.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - Warehouse Management System</title>
     <!-- Include Bootstrap for Modal Styling -->
+
+    <link rel="icon" type="image/png" href="<?= base_url('favicon.png'); ?>">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome for icon -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
