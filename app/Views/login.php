36f1f9878d856f5005969c0ee988028a0889dd32
deramos-michael <deramosmichael27@gmail.com>
2025-02-02 04:06:20 +0800

    S

--- app/Views/login.php
+++ app/Views/login.php
@@ -27,7 +27,7 @@
             border-radius: 10px;
             box-shadow: 0 4px 16px rgba(0, 0, 0, 0.1);
             width: 100%;
-            max-width: 380px;
+            max-width: 400px;
             transition: all 0.3s ease-in-out;
             animation: fadeIn 1s ease-in-out;
             transition: all 0.3s ease-in-out;
