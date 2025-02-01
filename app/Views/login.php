3aa38af449e84ff0910f5e2f8ec62214e89394bf
deramos-michael <deramosmichael27@gmail.com>
2025-02-02 02:18:19 +0800

    s

--- app/Views/login.php
+++ app/Views/login.php
@@ -2,8 +2,8 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="icon" type="image/x-icon" href="<?= base_url('favicon.ico'); ?>">
-
+    
+    <link rel="icon" href="<?= base_url('favicon.ico'); ?>" type="image/x-icon">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - Warehouse Management System</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
