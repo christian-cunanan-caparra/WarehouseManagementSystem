51625fce2c5421dad28d86f27b3252ff4df16730
deramos-michael <deramosmichael27@gmail.com>
2025-02-02 14:42:55 +0800

    s

--- app/Views/login.php
+++ app/Views/login.php
@@ -157,7 +157,7 @@
             .container {
                 padding: 30px;
                 max-width: 85%;
-                margin-top: -70px;
+                margin-top: -60px;
             }
 
             h1 {
