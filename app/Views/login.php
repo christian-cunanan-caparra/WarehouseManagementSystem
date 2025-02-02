4f027c6685e0e886af3ce79dc300f8d6ffe85184
deramos-michael <deramosmichael27@gmail.com>
2025-02-02 14:46:55 +0800

    s

--- app/Views/login.php
+++ app/Views/login.php
@@ -153,7 +153,7 @@
 
         /* Responsive fixes */
         
-        @media (max-width: 900px) {
+        @media (max-width: 400px) {
             .container {
                 padding: 30px;
                 max-width: 85%;
